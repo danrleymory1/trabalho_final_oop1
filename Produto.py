4682fb3774299568bb2e49db836eb29c53c0b656
vinicius <viniciusr66.vr@gmail.com>
2023-06-22 10:46:10 -0300

    adicionar métodos de acesso e validação na classe Produto

--- Produto.py
+++ Produto.py
@@ -8,6 +8,29 @@ class Produto:
         self.preco = preco
         self.quantidade = quantidade
 
+    def get_codigo(self):
+        return self.codigo
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, novo_nome):
+        self.nome = novo_nome
+
+    def get_preco(self):
+        return self.preco
+
+    def set_preco(self, novo_preco):
+        if novo_preco.isnumeric():
+            self.preco = float(novo_preco)
+
+    def get_quantidade(self):
+        return self.quantidade
+
+    def set_quantidade(self, nova_quantidade):
+        if novo_quantidade.isnumeric():
+            self.quantidade = int(nova_quantidade)
+
     def exibir_informacoes(self):
         print(f"Código: {self.codigo}")
         print(f"Nome: {self.nome}")

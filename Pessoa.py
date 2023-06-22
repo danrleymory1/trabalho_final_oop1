af9e07a636f3705d48d014593b6c36a63a1ceb4e
vinicius <viniciusr66.vr@gmail.com>
2023-06-22 11:09:49 -0300

    implementação de herança e validação de parametros nas classes relacionadas a pessoa

--- Pessoa.py
+++ Pessoa.py
@@ -1,21 +1,72 @@
-class PessoaFisica:
+from bradocs4py import validar_cpf, validar_cnpj
+
+
+class Pessoa:
+    def __init__(self, nome, endereco):
+        self.nome = nome
+        self.endereco = endereco
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, novo_nome):
+        if len(novo_nome) > 0:
+            self.nome = novo_nome
+        else:
+            print("Nome inválido. Tente novamente")
+
+    def get_endereco(self):
+        return self.endereco
+
+    def set_endereco(self, novo_endereco):
+        if len(novo_endereco) > 0:
+            self.endereco = novo_endereco
+        else:
+            print("Endereço inválido. Tente novamente")
+
+
+class PessoaFisica(Pessoa):
     def __init__(self, nome, cpf, endereco):
         self.cpf = cpf
-        self.nome = nome
-        self.endereco = endereco
+        self.super().__init__(nome, endereco)
+
+    def get_cpf(self):
+        return self.cpf
+
+    def set_cpf(self, novo_cpf):
+        if validar_cpf(novo_cpf):
+            self.cpf = novo_cpf
+        else:
+            print("CPF inválido. Tente novamente")
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, novo_nome):
+        if len(novo_nome) > 0:
+            self.nome = novo_nome
+        else:
+            print("Nome inválido. Tente novamente")
 
 
-class PessoaJuridica:
+class PessoaJuridica(Pessoa):
     def __init__(self, nome, cnpj, endereco):
-        self.nome = nome
         self.cnpj = cnpj
-        self.endereco = endereco
+        self.super().__init__(nome, endereco)
+
+    def get_cnpj(self):
+        return self.cnpj
+
+    def set_cnpj(self, novo_cnpj):
+        if validar_cnpj(novo_cnpj):
+            self.cnpj = novo_cnpj
+        else:
+            print("CNPJ inválido. Tente novamente")
 
     def aplicar_desconto(self, valor_total):
         desconto = valor_total * 0.05  # 5% de desconto
         valor_com_desconto = valor_total - desconto
         return valor_com_desconto
-
 
 
 class Funcionario(PessoaFisica):
@@ -26,5 +77,3 @@ class Funcionario(PessoaFisica):
         desconto = valor_total * 0.1  # 10% de desconto
         valor_com_desconto = valor_total - desconto
         return valor_com_desconto
-
-

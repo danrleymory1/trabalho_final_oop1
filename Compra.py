2a1c1757f16c006db2338f471e463f0db7ca186f
vinicius <viniciusr66.vr@gmail.com>
2023-06-22 11:35:03 -0300

    adicionar gets e sets na classe Nota Fiscal

--- Compra.py
+++ Compra.py
@@ -5,11 +5,25 @@ class NotaFiscal:
         self.produtos = produtos
         self.valor_total = valor_total
 
+    def get_tipo(self):
+        return self.tipo
+
+    def get_cliente(self):
+        return self.cliente
+
+    def get_produtos(self):
+        return self.produtos
+
+    def get_valor_total(self):
+        return self.valor_total
+
     def exibir_dados(self):
         print("=== Nota Fiscal ===")
         print(f"Tipo: {self.tipo}")
         print(f"Cliente: {self.cliente.nome}")
         print("Produtos:")
         for produto, quantidade in self.produtos.items():
-            print(f"Nome: {produto.nome}, Quantidade: {quantidade}, Preço: R$ {produto.preco:.2f}")
+            print(
+                f"Nome: {produto.nome}, Quantidade: {quantidade}, Preço: R$ {produto.preco:.2f}"
+            )
         print(f"Valor Total: R$ {self.valor_total:.2f}")
